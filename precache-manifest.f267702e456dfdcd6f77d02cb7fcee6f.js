9f4772e6bcbe5af893a6be43109624e7ff61600f
param patil <paramrpatil@gmail.com>
2023-03-22 23:18:25 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.f267702e456dfdcd6f77d02cb7fcee6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c026e64eb5dafb466998",
+    "url": "/healthsure-frontend/static/css/main.1337dbf1.chunk.css"
+  },
+  {
+    "revision": "c026e64eb5dafb466998",
+    "url": "/healthsure-frontend/static/js/main.c026e64e.chunk.js"
+  },
+  {
+    "revision": "c51dca0aff96b0e51c99",
+    "url": "/healthsure-frontend/static/css/1.3b416db9.chunk.css"
+  },
+  {
+    "revision": "c51dca0aff96b0e51c99",
+    "url": "/healthsure-frontend/static/js/1.c51dca0a.chunk.js"
+  },
+  {
+    "revision": "04bd1bc4c563b25dba7c",
+    "url": "/healthsure-frontend/static/js/runtime~main.04bd1bc4.js"
+  },
+  {
+    "revision": "9299fbfb8d5b5fcceba9b99c7bfd8733",
+    "url": "/healthsure-frontend/index.html"
+  }
+];
